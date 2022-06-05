a74484c1c30463fcda8caedb408b6da5268ba733
ongyongen <ongyongen2021@gmail.com>
2022-06-06 01:27:52 +0800

    restructured code to improve readability

--- scripts.js
+++ scripts.js
@@ -4,11 +4,13 @@ let opButtons = document.querySelectorAll('p.op-buttons')
 let clearButton = document.querySelector('div#clear')
 let deletePrevButton = document.querySelector('div#delete')
 
+// Store state of computation
 let opArray = ['+', '-', '/', 'x', '=']
 let computation = []
 let computationVar = document.querySelector('p#computation')
 let result = document.querySelector('p#final-result')
 
+// Store keybind to cursor click mapping
 const keybindMapping = {
     "0" : 48,
     "1" : 49,
@@ -30,7 +32,7 @@ const keybindMapping = {
     "delete" : 8
 }
 
-
+// Add basic functions
 function add(num1, num2) {
     return num1 + num2
 }
@@ -51,20 +53,27 @@ function operate(op, num1, num2) {
     return op(num1, num2)
 }
 
+// Add functions associated with buttons
 function selectDigitButton(e) {
+
+    // Concat >= 2 digits together
+    let currentInput = e.target.textContent
+    let lastArrInput = computation[computation.length-1]
     if(computation.length != 0 & 
-        !opArray.includes(computation[computation.length-1]) &
-        !opArray.includes(e.target.textContent)) {
-        computation[computation.length-1] = computation[computation.length-1] + e.target.textContent
+        !opArray.includes(lastArrInput) &
+        !opArray.includes(currentInput)) {
+        computation[computation.length-1] = lastArrInput + currentInput
     } else {
-        computation.push(e.target.textContent)
+        computation.push(currentInput)
     }
     
+    // Generate calculator screen
     computationVar.textContent = computation.join(' ') // remove ',' when concat array
     if (!opArray.includes(e.target.textContent)) {
         result.textContent = computation[computation.length-1]
     }
 
+    // Prevent division by 0
     if(computation[1] == "/" & computation[2] == 0) {
         alert("Cannot divide by zero!")
         computation.pop()
@@ -95,15 +104,16 @@ function selectOpButton(e) {
         }
 
         let res = operate(op, Number(computation[0]), Number(computation[2]));
-        res.toString().includes('.') == true ? res = res.toFixed(2) : res
-        result.textContent = res;
+        res.toString().includes('.') == true ? res = res.toFixed(2) : res // round up decimals to 2 dp 
         
+        // Generate calculator screen
         if (computation[3] != '=') {
             computation = [res, computation[3]]
         } else {
             computation = [res]
         }
         computationVar.textContent = computation.join(' ') // remove ',' when concat array
+        result.textContent = res;
     }
 }
 
@@ -117,13 +127,16 @@ function selectClearButton(e) {
 function selectDeletePrevButton(e) {
    computation.pop()
    computationVar.textContent = computation.join(' ')
-
    if (opArray.includes(computation[computation.length-1])) {
        result.textContent = ' '
    } else {
        result.textContent = computation[computation.length-1]
    }
 }
+
+// Add event listeners 
+clearButton.addEventListener('click', selectClearButton)
+deletePrevButton.addEventListener('click', selectDeletePrevButton)
 
 digitButtons.forEach((button) => {
     button.addEventListener('click', selectDigitButton)
@@ -133,24 +146,6 @@ opButtons.forEach((button) => {
     button.addEventListener('click', selectOpButton)
 })
 
-clearButton.addEventListener('click', selectClearButton)
-
-
-deletePrevButton.addEventListener('click', selectDeletePrevButton)
-
-
-
-/*
-document.addEventListener('keydown', function(event) {
-    if(event.keyCode == 8) {
-        clearButton.click()
-    } else if (event.keyCode == 49) {
-        test.click()
-    }
-});
-*/
-
-
 document.addEventListener('keydown', function(event) {
     for (const [key, value] of Object.entries(keybindMapping)) {
         let keyboardMapping = document.getElementById(key)
@@ -159,4 +154,3 @@ document.addEventListener('keydown', function(event) {
         }
     }
 })
-
